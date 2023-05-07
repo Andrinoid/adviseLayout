753f1d911b6e17615d9a33574ed954c6f7d69674
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-05-07 11:49:01 -0300

    Make footer position fixed.

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -61,6 +61,11 @@ const Footer = styled.footer`
     height: 38px;
     background-color: #f8fafb;
     box-shadow: inset 0px 1px 0px #e8eaed;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
 `;
 
 const SiderContainer = styled.div`
